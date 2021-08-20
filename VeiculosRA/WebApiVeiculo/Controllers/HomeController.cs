652470e8abe0ebe2668f76f74e400807e2830656
Alex Junior <alexjuniorkuss@outlook.com>
2021-08-20 15:26:30 -0300

    Done back and test with links for push json

---
+++ VeiculosRA/WebApiVeiculo/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Data.Models;
+using Data.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiVeiculo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController<T, R> : ControllerBase where T : BaseModel where R : BaseRepository<T>
+    {
+        R repo = Activator.CreateInstance<R>();
+        
+        [HttpPost]
+        public void Post([FromBody] T model)
+        {
+            repo.Create(model);
+        }
+        [HttpGet]
+        public List<T> Get()
+        {
+            return repo.Read();
+        }
+        [HttpGet("{Id}")]
+        public T Get(int id)
+        {
+            return repo.Read(id);
+        }
+        [HttpPut("{Id}")]
+        public void Put(int id, [FromBody] T model)
+        {
+            model.Id = id;
+            repo.Update(model);
+        }
+        [HttpDelete("{Id}")]
+        public void Delete(int id)
+        {
+            repo.Delete(id);
+        }
+
+    }
+}
---
+++ VeiculosRA/WebApiVeiculo/Controllers/VeiculosController.cs
@@ -0,0 +1,15 @@
+﻿using Data.Models;
+using Data.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiVeiculo.Controllers
+{
+    public class VeiculosController : HomeController<Veiculos, VeiculoRepository>
+    {
+    }
+}
